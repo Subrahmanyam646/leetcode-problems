b1ba2ef4180e0e295956aa6ef02bf96e3ad67d95
Subrahmanyam646 <59686564+Subrahmanyam646@users.noreply.github.com>
2024-10-17 00:28:47 +0530

    Time: 16 ms (50.36%) | Memory: 42.8 MB (77.52%) - LeetSync

--- 837-most-common-word/most-common-word.java
+++ 837-most-common-word/most-common-word.java
@@ -1,11 +1,13 @@
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
         Map<String, Integer> map = new HashMap<>();
-        paragraph = paragraph.replace(","," ").replace("  "," ");
-        String [] words = paragraph.split(" ");
+        String normalizedStr = paragraph.replaceAll("[!?',;\\.]", " ").toLowerCase();
+        
+        // Step 2: Split the normalized string into words.
+        String[] words = normalizedStr.split("\\s+");
 
         for(String word: words){
-            word = word.replace("'","").replace("?","").replace(".","").replace("!","").replace(";","").toLowerCase();
+            //word = word.replace("'","").replace("?","").replace(".","").replace("!","").replace(";","").toLowerCase();
             map.put(word, map.getOrDefault(word, 0)+1);
         }
         int max = 0;
@@ -23,7 +25,6 @@ class Solution {
     }
     public boolean isItInBanned(String word, String [] banned){
         for(String ban: banned){
-            //ban = ban.replace(",","").replace(".","").toLowerCase();
             if(ban.equals(word)){
                 return true;
             }
