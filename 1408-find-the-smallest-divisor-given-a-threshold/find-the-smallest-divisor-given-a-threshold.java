6870eb9a89e98cd5b0b6d42845c7233d8fa4a3fc
Subrahmanyam646 <59686564+Subrahmanyam646@users.noreply.github.com>
2025-06-09 20:01:16 +0530

    Time: 29 ms (46.83%) | Memory: 47.7 MB (98.93%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -1,35 +1,29 @@
 class Solution {
+    public int findDivisor(int[] nums, int d) {
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            sum += Math.ceil((double)nums[i]/(double)d);
+        }
+        return sum;
+    }
     public int smallestDivisor(int[] nums, int threshold) {
-        //int [] minMax = minMaxOfArray(nums);
-        int low = 1;
-        int high =  minMaxOfArray(nums);
-
-        while(low < high){
-            int mid = (low + high) / 2;
-            int val = divideAllInArray(nums, mid);
-            if(val > threshold){
-                low = mid + 1;
-            }else{
-                high = mid;
-            }
+        int maxi = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > maxi) maxi = nums[i];
         } 
-        return low;
-    }
-    public int divideAllInArray(int[] nums, int mid){
-        int total = 0;
-        for(int i = 0; i < nums.length; i++){
-            total += Math.ceil((float)nums[i]/ mid);
-        }
-        return total;
-    }
-
-    public int minMaxOfArray(int[] nums){
-        int max = nums[0];
-        for(int i = 1; i< nums.length;i++){
-            if(nums[i] > max){
-                max = nums[i];
+        int low = 1, high = maxi;
+        int ans = 0;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            int divisorSum = findDivisor(nums, mid);
+            if(divisorSum <= threshold) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
             }
         }
-        return max;
+        return ans;
     }
 }
